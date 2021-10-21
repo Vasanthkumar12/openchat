43d4ebc4e7d2a6bde27dfb03ee67afcaee81b005
NITHI GD <g.d.nithi5@gmail.com>
2021-10-21 12:20:42 +0530

    Server changed vasanth merge to main

--- src/App.java
+++ src/App.java
@@ -3,13 +3,13 @@ import openchat.CallOnConnect;
 
 class ConnectionStatus implements CallOnConnect{
     public void onConnected(){
-        System.out.println("Connected to Server, can Start a chat now.");
+        System.out.println("Connected to Server 2, can Start a chat now.");
     }
 }
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Openchat app Started");
-        Client client = new Client(new ConnectionStatus());
+        Client client = new Client(new ConnectionStatus ());
     }
 }
     
